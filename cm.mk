59d33ffbefe62c682a8cfaaf93428dc0b47e96b6
Qwanor <Qwanor@gmail.com>
2016-10-21 08:46:03 +0300

    adb is disabled by default

--- cm.mk
+++ cm.mk
@@ -7,9 +7,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Call device specific makefile
 $(call inherit-product, device/highscreen/giraffe/cm_giraffe.mk)
 
-LOCAL_PATH := device/alps
+LOCAL_PATH := device/highscreen/giraffe
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0.1/MMB29M/2419427:user/release-keys PRIVATE_BUILD_DESC="alps-user 6.0.1 MMB29M 2419427 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0.1/MMB29M/2419427:user/release-keys PRIVATE_BUILD_DESC="giraffe-user 6.0.1 MMB29M 2419427 release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="giraffe"
@@ -38,13 +38,5 @@ CM_BUILD := giraffe
 
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp
-	
-PRODUCT_PROPERTY_OVERRIDES += \
-  persist.service.adb.enable=1 \
-  persist.service.debuggable=1 \
-  persist.sys.usb.config=mtp,adb
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
